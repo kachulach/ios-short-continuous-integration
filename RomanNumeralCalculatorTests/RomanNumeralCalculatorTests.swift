962865e274f1c1afa640747f306446c28d99a67c
Boris Kachulachki <kachulach@tazka-mac.local>
2017-11-29 00:10:48 +0100

    add toRoman unit tests

---
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -0,0 +1,40 @@
+//
+//  RomanNumeralCalculatorTests.swift
+//  RomanNumeralCalculatorTests
+//
+//  Created by Boris Kachulachki on 11/29/17.
+//  Copyright © 2017 buddybuild. All rights reserved.
+//
+
+import XCTest
+@testable import RomanNumeralCalculator
+
+class RomanNumeralCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testToRoman() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertEqual(toRoman(1), "I")
+        XCTAssertEqual(toRoman(2), "II")
+        XCTAssertEqual(toRoman(4), "IV")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
